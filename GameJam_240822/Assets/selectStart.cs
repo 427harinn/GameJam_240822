5775985c06087761b2cc34c42b3db495d00b8f20
guriguri00451 <guriguri00451@gmail.com>
2024-08-23 14:38:07 +0900

    音楽付け

---
+++ GameJam_240822/Assets/selectStart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KanKikuchi.AudioManager;
+public class selectStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGMManager.Instance.Play(BGMPath.SELECT_BGM);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
